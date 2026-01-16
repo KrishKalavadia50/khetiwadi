aa1abb4e7e6d456d9a92579bdbb1b088f7b49cd3
KrishKalavadia <krish55kalavadia@gmail.com>
2026-01-16 17:19:32 +0530

    Fix ESLint issues for Vercel build

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Moog from './components/Moog'
-import Til from './components/Til'
 
 function Main() {
   return (
     <div>
       <Moog />
-      {/* <Til /> */}
     </div>
   )
 }
